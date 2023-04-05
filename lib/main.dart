790023c835ef4329ba3801c79aa1df241728d79d
mohit <mohitsaini2882@gmail.com>
2023-04-05 18:33:52 +0530

    "just change primarySwatch color"

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: true,
       title: UtilStrings.appName,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
       initialRoute: Routes.signUpScreen,
       onGenerateRoute: Routes.generateRoute,
