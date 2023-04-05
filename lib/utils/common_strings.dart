53e53f2cf50aa722da4179502fcc62f71e707906
mohit <mohitsaini2882@gmail.com>
2023-04-05 18:38:26 +0530

    "just change the app name"

--- lib/utils/common_strings.dart
+++ lib/utils/common_strings.dart
@@ -1,4 +1,4 @@
 class UtilStrings{
-  static const String appName = "OM SOLUTION TODO APP";
+  static const String appName = "OM SOLUTION TODO APP New";
   static const String profilePicture = "https://res.cloudinary.com/dyofowjoa/image/upload/v1639381666/mohitsaini_vv0qxt.png";
 }
